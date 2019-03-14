ef600bab5153ffafbb47b36c643bad974a2bbd1b
eRgo35 <michal.czyz35@gmail.com>
2019-03-14 13:30:32 +0100

    Initial

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function dropHTML() {
+    $("#html-dropdown").slideToggle("fast");
+    $("#1").toggleClass("fa-sort-up fa-sort-down");
+}
+function dropCSS() {
+    $("#css-dropdown").slideToggle("fast");
+    $("#2").toggleClass("fa-sort-up fa-sort-down");
+}
+function dropHTML5() {
+    $("#html5-dropdown").slideToggle("fast");
+    $("#3").toggleClass("fa-sort-up fa-sort-down");
+}
+function dropDOCS() {
+    $("#docs-dropdown").slideToggle("fast");
+    $("#4").toggleClass("fa-sort-up fa-sort-down");
+}
+function dropMEDIA() {
+    $("#media-dropdown").slideToggle("fast");
+    $("#5").toggleClass("fa-sort-up fa-sort-down");
+}
+function dropLINKS() {
+    $("#links-dropdown").slideToggle("fast");
+    $("#6").toggleClass("fa-sort-up fa-sort-down");
+}
+
