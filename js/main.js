8faa591962997bc660faa39433b0a61ab331b302
eRgo35 <michal.czyz35@gmail.com>
2019-03-14 18:23:21 +0100

    Final Theme Engine

--- js/main.js
+++ js/main.js
@@ -1,23 +1,48 @@
+$(document).ready(function () {
+    if (document.cookie.split(';').filter((item) => item.includes('theme=blue')).length) {
+        document.getElementById('theme').href = 'css/blue.css';
+        $('.fa-check-circle-o').css('display', 'none');
+        $('.Blue').css('display', 'block');
+    } else if (document.cookie.split(';').filter((item) => item.includes('theme=green')).length) {
+        document.getElementById('theme').href = 'css/green.css';
+        $('.fa-check-circle-o').css('display', 'none');
+        $('.Green').css('display', 'block');
+    } else if (document.cookie.split(';').filter((item) => item.includes('theme=red')).length) {
+        document.getElementById('theme').href = 'css/red.css';
+        $('.fa-check-circle-o').css('display', 'none');
+        $('.Red').css('display', 'block');
+    } else if (document.cookie.split(';').filter((item) => item.includes('theme=yellow')).length) {
+        document.getElementById('theme').href = 'css/yellow.css';
+        $('.fa-check-circle-o').css('display', 'none');
+        $('.Yellow').css('display', 'block');
+    }
+});
+
 function dropHTML() {
     $("#html-dropdown").slideToggle("fast");
     $("#1").toggleClass("fa-sort-up fa-sort-down");
 }
+
 function dropCSS() {
     $("#css-dropdown").slideToggle("fast");
     $("#2").toggleClass("fa-sort-up fa-sort-down");
 }
+
 function dropHTML5() {
     $("#html5-dropdown").slideToggle("fast");
     $("#3").toggleClass("fa-sort-up fa-sort-down");
 }
+
 function dropDOCS() {
     $("#docs-dropdown").slideToggle("fast");
     $("#4").toggleClass("fa-sort-up fa-sort-down");
 }
+
 function dropMEDIA() {
     $("#media-dropdown").slideToggle("fast");
     $("#5").toggleClass("fa-sort-up fa-sort-down");
 }
+
 function dropLINKS() {
     $("#links-dropdown").slideToggle("fast");
     $("#6").toggleClass("fa-sort-up fa-sort-down");
@@ -26,9 +51,9 @@ function dropLINKS() {
 function themeSelect() {
     var x = document.getElementById("theme-change");
     if (x.style.display === "grid") {
-      x.style.display = "none";
+        x.style.display = "none";
     } else {
-      x.style.display = "grid";
+        x.style.display = "grid";
     }
 }
 
@@ -37,22 +62,29 @@ function themeBlue() {
     $('.fa-check-circle-o').css('display', 'none');
     $('.Blue').css('display', 'block');
     document.getElementById("theme-change").style.display = "none";
+    document.cookie = "theme=blue; expires=Fri, 31 Dec 9999 23:59:59 GMT";
 }
+
 function themeGreen() {
     document.getElementById('theme').href = 'css/green.css';
     $('.fa-check-circle-o').css('display', 'none');
     $('.Green').css('display', 'block');
     document.getElementById("theme-change").style.display = "none";
+    document.cookie = "theme=green; expires=Fri, 31 Dec 9999 23:59:59 GMT";
 }
+
 function themeRed() {
     document.getElementById('theme').href = 'css/red.css';
     $('.fa-check-circle-o').css('display', 'none');
     $('.Red').css('display', 'block');
     document.getElementById("theme-change").style.display = "none";
+    document.cookie = "theme=red; expires=Fri, 31 Dec 9999 23:59:59 GMT";
 }
+
 function themeYellow() {
     document.getElementById('theme').href = 'css/yellow.css';
     $('.fa-check-circle-o').css('display', 'none');
     $('.Yellow').css('display', 'block');
     document.getElementById("theme-change").style.display = "none";
+    document.cookie = "theme=yellow; expires=Fri, 31 Dec 9999 23:59:59 GMT";
 }
